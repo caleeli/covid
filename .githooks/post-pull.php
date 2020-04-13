ffdd34c118ea6db3bbf2816bacb06ffa3a4cfe8d
David Callizaya <davidcallizaya@gmail.com>
2020-04-12 20:11:28 -0400

    change to prod mode

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -3,7 +3,7 @@ run(
     // Update libraries
     onchange(['composer.json'], 'composer install;') .
     // Update front end
-    onchange(['resources/js','resources/sass','resources/images'], 'npm run dev;') .
+    onchange(['resources/js','resources/sass','resources/images'], 'npm run prod;') .
     // Rebuild database
     onchange(['database/migrations'], 'composer dumpautoload;php artisan migrate;')
 );
